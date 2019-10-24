9e1976716a417279f4e67a7bb56c643e90905828
Aleksey <al.nkow@gmail.com>
2019-10-24 16:05:32 +0300

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`=== SERVER STARTED ON PORT ${PORT} ===`);
+});
+
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+
+
+
