37ac90297afbc6459add96536527879970c1071d
Aleksey <al.nkow@gmail.com>
2020-01-26 12:05:30 +0200

    import

--- controllers/import.js
+++ controllers/import.js
@@ -23,81 +23,27 @@ exports.deleteAllProductsAndPositions = async (req, res) => {
  * SAVE IMPORTED DATA TO SERVER
  */
 exports.publish = async (req, res) => {
-  // !!!!!!!!!! Везде TRY CATCH !!!!!!!!!!!!!!!
   const importedProducts = req.body.data;
-  const appShops = await Shop.find();
-
-  // const currentProduct = { ...req.body.data[0].product };
-  // await AddCategoryToProduct(categories, currentProduct);
-
-  // Вывод результата
-  // return res.status(200).json({
-  //   appShops,
-  // });
 
   try {
     for (const item of importedProducts) {
       const currentProduct = item.product;
-      await AddCategoryToProduct(currentProduct);
+
+      currentProduct.category = await getCategoryId(
+        currentProduct.category,
+      );
+
       const newProduct = new Product({
         ...currentProduct,
         _id: new mongoose.Types.ObjectId(),
       });
       const createdProduct = await newProduct.save();
 
-      // =============================
-      // ===== CREATE POSITIONS ======
-      // =============================
-      const currentShops = item.shops;
-      if (currentShops && Object.keys(currentShops).length) {
-        for (const shop of appShops) {
-          const { key } = shop;
-          if (key) {
-            const currentPosition = currentShops[key];
-            if (currentPosition) {
-              const newPosition = new Position({
-                productId: createdProduct._id,
-                shopId: shop._id,
-                article: currentPosition[key + 'Art'],
-                price: currentPosition[key + 'Price'],
-                link: currentPosition[key + 'Link'],
-                _id: new mongoose.Types.ObjectId(),
-              });
-              await newPosition.save();
-            }
-          }
-        }
-      }
-      // =============================
-      // =============================
+      await createPositions(item.shops, createdProduct._id);
     }
   } catch (err) {
     return res.status(500).json({ error: err });
   }
-
-  // Вывод результата
-  // return res.status(200).json({
-  //   importedProducts,
-  // });
-
-  // !!!! Проверять на обязательные поля - если нет, то не сохранять этот товар! - в ответе вернуть массив
-  // товаров которые не соответствуют требованиям
-  // 1) name !!!!
-  // 2) category
-
-  // !!!!! В ТАБЛИЦЕ НА ФРОНТЕ подсвечивать ячейки где ошибка !!!!
-  // и Вывести сообщение что такие то товары не подходят
-
-  /**
-   * В цикле проходим весь массив
-   * 1. если нет категории в базе - сохраняем категорию
-   * 2. Схраняем продукт и получаем его айди
-   * 3. Сохраняем ассортимент в магазины
-   */
-
-  // res
-  //   .status(201)
-  //   .json({ message: 'Product created', id: created._id });
 
   return res
     .status(201)
@@ -107,10 +53,9 @@ exports.publish = async (req, res) => {
 /**
  * Add category ID to the new product
  */
-async function AddCategoryToProduct(product) {
-  if (!product.category) return;
+async function getCategoryId(currentProductCategoryName) {
+  if (!currentProductCategoryName) return;
   const categories = await Category.find();
-  const { category: currentProductCategoryName } = product;
 
   const foundCategory = categories.find(itemCategory => {
     return (
@@ -119,7 +64,7 @@ async function AddCategoryToProduct(product) {
     );
   });
 
-  product.category = foundCategory
+  return foundCategory
     ? foundCategory._id
     : await createNewCategory(currentProductCategoryName);
 }
@@ -135,4 +80,50 @@ async function createNewCategory(name) {
     _id: new mongoose.Types.ObjectId(),
   });
   return await newCategory.save();
+}
+
+/**
+ * Create a new position
+ */
+async function createNewPosition(
+  productId,
+  shopId,
+  article,
+  price,
+  link,
+) {
+  const newPosition = new Position({
+    productId,
+    shopId,
+    article,
+    price,
+    link,
+    _id: new mongoose.Types.ObjectId(),
+  });
+  return await newPosition.save();
+}
+
+/**
+ * Create positions for product item
+ */
+async function createPositions(currentShops, createdProductId) {
+  if (currentShops && Object.keys(currentShops).length) {
+    const appShops = await Shop.find();
+
+    for (const shop of appShops) {
+      const { key } = shop;
+      if (key) {
+        const currentPosition = currentShops[key];
+        if (currentPosition) {
+          await createNewPosition(
+            createdProductId,
+            shop._id,
+            currentPosition[key + 'Art'],
+            currentPosition[key + 'Price'],
+            currentPosition[key + 'Link'],
+          );
+        }
+      }
+    }
+  }
 }
